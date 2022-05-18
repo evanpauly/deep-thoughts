a7399d591282015fd574e7bebd09d99e9dcde70d
evanpauly <94558036+evanpauly@users.noreply.github.com>
2022-05-18 12:13:33 -0500

    fourth part done

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,19 +1,22 @@
 import { gql } from '@apollo/client';
 
-export const QUERY_THOUGHTS = gql`
-    query thoughts($username: String) {
-    thoughts(username: $username) {
+export const QUERY_USER = gql`
+    query user($username: String!) {
+    user(username: $username) {
+      _id
+      username
+      email
+      friendCount
+      friends {
+        _id
+        username
+      }
+      thoughts {
         _id
         thoughtText
         createdAt
-        username
         reactionCount
-        reactions {
-        _id
-        createdAt
-        username
-        reactionBody
-        }
+      }
     }
-}
+  }
 `;
